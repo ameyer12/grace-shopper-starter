fe5d314c36228a1976e4e8b1457270c0690fc32a
Ethan <ekhuang12@gmail.com>
2022-11-03 16:46:24 -0700

    added public, src, and components folder for structure

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
+
 const apiRouter = express.Router();
 
 const productsRouter = require('./productsRouter');
 apiRouter.use('/products', productsRouter);
+
+//
 
 module.exports = apiRouter;
