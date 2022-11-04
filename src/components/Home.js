5dbea020da8c62e1db8c2c5362d50ac1bda453db
Ethan <ekhuang12@gmail.com>
2022-11-03 18:20:58 -0700

    style.css for everything

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const Home = () => {
+    return (
+        <div className="home" >
+        <h1  className="home">Grace Shopper</h1>
+        <img className="home" src="https://media.istockphoto.com/vectors/shopping-cart-icon-isolated-on-white-background-vector-id1206806317"></img>
+    </div>
+    )
+}
+
+
+export default Home;
