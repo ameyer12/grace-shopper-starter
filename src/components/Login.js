a494cbb52fd5791f953ca58761330b6a737f7dfa
Ethan <ekhuang12@gmail.com>
2022-11-05 11:57:16 -0500

    login and register should be done, placeholder items in // and /* */

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,60 @@
 import React from 'react';
 import './style.css';
+import { loginUser } from "../api";
+import { Snackbar } from "@mui/material";
 
+const Login = ({setUsername, username, setPassword, password, navigate, setToken, /*setNewPageThatLoadsWithLogin,*/ setOpen, open, token}) => {
+
+    const handleSubmit = async () => {
+        const results = await loginUser(username, password);
+        console.log('results submitting user: ', results)
+
+        
+        if (results.token) {
+            setToken(results.token)
+            window.localStorage.setItem('token', results.token) 
+            const userResults = await getMe(results.token)
+            console.log ('token is: ', results.token, 'user is: ', userResults.username)
+            /*setNewPageThatLoadsWithLogin*/ (await /*getNewPageThatLoadsWithLogin*/(results.token, username))
+            // navigate('/NewPageThatLoadsWithLogin') 
+        } else {
+            setOpen(true)
+            let form = document.querySelector('form')
+            form.reset()
+        }
+    }
+    
+    return (
+        <form className='loginForm' onSubmit = {(event)=> {
+            event.preventDefault();
+                handleSubmit()
+            }
+        }>
+            <Snackbar 
+                open={open}
+                anchorOrigin= {{vertical: "top", horizontal: "center"}}
+                message='Username or Password is incorrect, please try again' 
+                autoHideDuration={2000} 
+                onClose={() => setOpen(false)}
+                >
+            </Snackbar>
+            <div className="loginForm">
+            <input className="usernameEntry"
+            type='text'
+            placeholder="Enter Username"
+            onChange={(event) => setUsername(event.target.value)}>
+            </input>
+            <input className="passwordEntry"
+            type='password'
+            placeholder="Enter Password"
+            onChange={(event) => setPassword(event.target.value) }>
+            </input>
+
+
+            <button type='submit'>Submit</button>
+            </div>
+        </form>
+    )
+}
 
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,82 @@
 import React from 'react';
 import './style.css';
+import { registerUser, /*getNewPageThatLoadsWithLogin*/ } from "../api";
+import { Snackbar } from "@mui/material";
+
+const Register = ({ setToken, navigate, setUsername, username, setPassword, password, /*setNewPageThatLoadsWithLogin,*/ setOpen, open, }) => {
+
+
+    const handleSubmit = async () => {
+        const passwordRequirements = new RegExp ("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$")
+        // Lowercase, Capital, number, special character  //
+
+        if ((password.length >= 6) && (passwordRequirements.test(password))) {
+            
+            const results = await registerUser(username, password);
+            console.log('register results: ', results)
+            console.log('password is: ', password, 'user is: ', username)
+            
+            if (results.token) {
+                setToken(results.token)
+                window.localStorage.setItem('token', results.token) 
+                const userResults = await /*getNewPageThatLoadsWithLogin*/(results.token, username)
+                let users/*personalizedData*/ = userResults.data
+                /*setNewPageThatLoadsWithLogin*/(users/*NewPageThatLoadsWithLogin*/)
+                // navigate('/NewPageThatLoadsWithLogin')
+            } else {
+                console.log("error registering user")
+            }
+        } else {
+            let form = document.querySelector("form")
+            setOpen(true)
+            form.reset()
+            setUsername('')
+            setPassword('') 
+
+        }
+    }
+
+
+    return (
+        <form className='registerForm' onSubmit = {(event)=> {
+            event.preventDefault()
+                handleSubmit()
+            }
+            }>
+            <Snackbar 
+                open={open}
+                anchorOrigin= {{vertical: "top", horizontal: "center"}}
+                message='Password does not meet requirements, please try again' 
+                autoHideDuration={2000} 
+                onClose={() => setOpen(false)}
+                >
+            </Snackbar>
+            <div className="enterInfoRegister">
+            <input className="setUsernameEntry"
+            type='text'
+            placeholder="Enter Username"
+            onChange={(event) => setUsername(event.target.value)}>
+            </input>
+            <input className="setPasswordEntry"
+            //make type password to use dots when entered
+            type='password'
+            placeholder="Enter Password"
+            onChange={(event) => setPassword(event.target.value) }>
+            </input>
+            <h4>Password Requirements:</h4>
+            <ul className="passwordRulesList">
+                <li className="passwordRule">Must be at least 6 characters in length</li>
+                <li className="passwordRule">Must contain a capital letter</li>
+                <li className="passwordRule">Must contain a lowercase letter</li>
+                <li className="passwordRule">Must include a number</li>
+                <li className="passwordRule">Must include a special character</li>
+            </ul>
+
+            <button type='submit'>Submit</button>
+            </div>
+        </form>
+    )
+}
 
 
 export default Register;
