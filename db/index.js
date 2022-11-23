2a077e70379b4eda9d6781177e86d5266eb63805
Austin <austinmeyer1106@gmail.com>
2022-11-22 22:43:30 -0600

    debugging

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const { DATABASE_URL } = process.env;
 
 const client = new Pool({
-  connectionString: DATABASE_URL || 'postgres://localhost:5432/graceshopper'
+  connectionString: DATABASE_URL || 'https://localhost:5432/graceshopper'
 })
 
 module.exports = { client }
