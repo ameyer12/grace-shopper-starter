e5fe66a0f0907bc2b223cd22683ca84eec0caaf2
Austin <austinmeyer1106@gmail.com>
2022-12-01 19:59:42 -0600

    added css for single product page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/navbar";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
-<<<<<<< HEAD
 import { Shop, Login, Register, SingleProduct } from './components';
 import { getProducts, loginUser, registerUser, getSingleProduct } from "./api"
-=======
-import { Shop, Login } from './components';
-import { getProducts, loginUser } from "./api"
-import SingleProductView from './components/SingleProductView';
->>>>>>> 21dc137c61d1fb81261493a7bb3e824688062091
+
 
 const App = () => {
 
@@ -35,10 +30,9 @@ const App = () => {
           <Routes>
               <Route path="/" element={<Home navigate={navigate} />} />
               <Route path="/shop" element={<Shop products={products} />} />
-              <Route path="/shop/product/:productId" element={<SingleProductView products={products} />} />
               <Route path="/login" element={<Login loginUser={loginUser} navigate={navigate} />} />
               <Route path="/register" element={<Register registerUser={registerUser} navigate={navigate} />} />
-              <Route path="products/:productId" element={<SingleProduct getSingleProduct={getSingleProduct} navigate={navigate} />} />
+              <Route path="/products/:productId" element={<SingleProduct getSingleProduct={getSingleProduct} navigate={navigate} />} />
           </Routes>
           <Footer />
       </div>
--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -31,9 +31,55 @@ const SingleProduct= ({getSingleProduct}) => {
     return (
         <div className="single-product-body">
             <img className="single-product-image" src={image} alt="Product image" width="500" height="600"/>
-            <h1 className="single-product-title">{title}</h1>
-            <p className="single-product-price">${price}</p>
-            <p className="single-product-description">{description}</p>
+            <div className="card" id="single-product-page-card">
+                <h1 className="single-product-title">{title}</h1>
+                <p className="single-product-price">${price}</p>
+                <div class="form-group">
+                    <label for="exampleFormControlSelect1">Select Size</label>
+                        <select class="form-control" id="select-size-form">
+                            <option>S</option>
+                            <option>M</option>
+                            <option>L</option>
+                            <option>XL</option>
+                            <option>XXL</option>
+                        </select>
+                </div>
+                <button 
+                type="submit"
+                id="add-to-cart-button" 
+                className="btn btn-primary"
+                onClick={(ev) => {
+                    ev.preventDefault();
+                    // handleRegister();
+                }}
+                >Add To Cart</button>
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                            Product Details
+                        </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <p>{description}</p>
+                        </div>
+                        </div>
+                    </div>
+                        <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Is this item in stock?
+                        </button>
+                        </h2>
+                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                           in stock goes here
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
